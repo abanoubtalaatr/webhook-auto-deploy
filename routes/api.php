eeb58631a3c2914b49a38a5460fd8c178262b2ab
Abanoub Talaat (NoubCode) <38631117+abanoubtalaatr@users.noreply.github.com>
2025-07-18 18:18:57 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();

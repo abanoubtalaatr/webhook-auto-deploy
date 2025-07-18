6798ec8016f80a7a9e9f54eaf97dc3f72932fd5c
Abanoub Talaat (NoubCode) <38631117+abanoubtalaatr@users.noreply.github.com>
2025-07-18 18:13:44 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/webhook-handler', function () {
+    // Run the deploy script
+    $process = new Process(['/bin/bash', '/home/digital07/auto-deploy.digital-vision-solutions.com/deploy.sh']);
+    
+    try {
+        $process->mustRun(); // This will throw an exception if the command fails
+    } catch (ProcessFailedException $exception) {
+        return response('Deployment failed: ' . $exception->getMessage(), 500);
+    }
+
+    return response('Deployment completed successfully.', 200);
+});
